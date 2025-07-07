f0d639f26ed4be0d0df0f79883d261fb12fa6661
chirag479 <officialchirag110@gmail.com>
2025-07-07 12:34:52 +0530

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const repo = 'Langsphere';
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +10,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 }
 
 export default nextConfig
